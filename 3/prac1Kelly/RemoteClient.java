b88722b5b8a8aa1b5ccff9858cbea5400bf6109d
David Berrospe Ramírez <david.berrospe.r03@gmail.com>
2022-05-18 21:01:21 -0500

    8th try.

--- 3/prac1Kelly/RemoteClient.java
+++ 3/prac1Kelly/RemoteClient.java
@@ -10,7 +10,7 @@ public class RemoteClient {
             printeRemote.metodo1();
             
         } catch (Exception e) {
-            //TODO: handle exception
+            e.printStackTrace();
         }
     }
 }
